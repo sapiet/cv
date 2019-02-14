ef416d5fccb8fc735de2c0cda75e7b26613acfd1
Xavier QUIEVRE <xavier.quievre@gmail.com>
2019-02-14 23:57:12 +0100

    [WIP]

--- src/DataFixtures/Data.php
+++ src/DataFixtures/Data.php
@@ -14,7 +14,7 @@ class Data
 		'mobile' => '07 81 25 65 74',
 		'website' => 'https://xavierquievre.fr',
 		'profession' => 'Développeur web',
-		'shortDescription' => 'Développeur <span>Web</span>, <span>Application</span> et <span>Mobile</span><br><a class="smoothscroll" href="#about">Scrollez</a> pour en savoir plus <a class="smoothscroll" href="#about">sur moi</a>.',
+		'shortDescription' => 'Développeur <span>Web</span>, <span>Application</span> et <span>Mobile</span><br> <a class="smoothscroll" href="#about">Scrollez</a> pour en savoir plus <a class="smoothscroll" href="#about">sur moi</a>.',
 		'longDescription' => 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
          eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
          voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
